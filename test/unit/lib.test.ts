eb606f3c8b41ae2e566be5f3297b20b7bb7a68a6
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 21:25:04 -0800

    Upgrade modules

--- test/unit/lib.test.ts
+++ test/unit/lib.test.ts
@@ -1,4 +1,4 @@
-// remove NODE_OPTIONS from ts-dev-stack
+// remove NODE_OPTIONS to not interfere with tests
 delete process.env.NODE_OPTIONS;
 
 import spawn from 'cross-spawn-cb';
@@ -57,7 +57,7 @@ function addTests(repo) {
       it('test:node', (done) => {
         mocha([], { cwd: dest }, (err?: Error): undefined => {
           if (err) {
-            done(err.message);
+            done(err);
             return;
           }
           done();
